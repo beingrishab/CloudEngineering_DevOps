0d67186f193fae46fc619b1afaf6c76a6c01fad2
beingrishab <32215395+beingrishab@users.noreply.github.com>
2024-03-03 02:52:16 +0530

    Update Get-MetaData.ps1

--- Challenge 2/Get-MetaData.ps1
+++ Challenge 2/Get-MetaData.ps1
@@ -6,6 +6,6 @@ $sp.PasswordCredentials.SecretText
 $pscredential = Get-Credential -UserName $sp.AppId
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenantId
 
-
 $SecureStringPwd = $sp.PasswordCredentials.SecretText | ConvertTo-SecureString -AsPlainText -Force
-$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sp.AppId, $SecureStringPwd
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenantId
