9acc994323c42876cf387755e6e46f11f7a83e3b
beingrishab <32215395+beingrishab@users.noreply.github.com>
2024-03-03 02:51:51 +0530

    Update Get-MetaData.ps1

--- Challenge 2/Get-MetaData.ps1
+++ Challenge 2/Get-MetaData.ps1
@@ -1,1 +1,11 @@
+$sp = New-AzADServicePrincipal -DisplayName ServicePrincipalName
 
+# Retrieve the plain text password for use with Get-Credential in the next command.
+$sp.PasswordCredentials.SecretText
+
+$pscredential = Get-Credential -UserName $sp.AppId
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenantId
+
+
+$SecureStringPwd = $sp.PasswordCredentials.SecretText | ConvertTo-SecureString -AsPlainText -Force
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential -
